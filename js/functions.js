5595affd3352ed77f5127bf98cd26b763354a380
Lice <mrtime22@bk.ru>
2023-10-19 00:17:19 +0300

    Внёс правки в функции

--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,24 @@
-let first_str;
-let maxlenght;
-let palindrom;
-let string_whith_number;
-
 function checkLenght(first_str,maxlenght){
-  return console.log(first_str.length <= maxlenght);
+  return first_str.length <= maxlenght;
 }
 
 checkLenght('zappp',5);
 
 function isPalindrom(palindrom){
   palindrom = palindrom.replace(/\s/g, "");
-  return console.log(palindrom.split("").reverse().join("") == palindrom);
+  return palindrom.split("").reverse().join("") == palindrom;
 }
 
 isPalindrom('лёша на полке клопа нашёл');
 
-function thiefOfNumbers(string_whith_number){
-  let counter1 = "";
-  for(i = 0; i < string_whith_number.length; i++){
-    let counter2;
-    if(string_whith_number[i] >= '0' && string_whith_number[i] <= '9' ){
-      counter2 = +string_whith_number[i];
-      counter1 += counter2;
-    }else{
-      counter2 = NaN;
-    }
+function stealANumber(stringWhithNumber) {
+	let counter1 = "";
+	for (i = 0; i < stringWhithNumber.length; i++) {
+	  if (stringWhithNumber[i] >= '0' && stringWhithNumber[i] <= '9') {
+		counter1 += +stringWhithNumber[i];
+	  }
+	}
+	console.log(counter1);
   }
-  console.log(counter1);
-}
 
-thiefOfNumbers('-21об');
+stealANumber('агент 007');
